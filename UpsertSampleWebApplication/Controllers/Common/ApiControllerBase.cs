bb7daa3430a36e80fcf156e3222d4e68d30e7cfa
Alexandr Tolstuhin <alex.tolstuchin@gmail.com>
2021-08-12 20:12:49 +0500

    extract ApiControllerBase

---
+++ UpsertSampleWebApplication/Controllers/Common/ApiControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UpsertSampleWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        protected readonly IMediator Mediator;
+
+        protected ApiControllerBase(IMediator mediator)
+        {
+            Mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+    }
+}
--- UpsertSampleWebApplication/Controllers/TodoController.cs
+++ UpsertSampleWebApplication/Controllers/TodoController.cs
@@ -7,34 +7,24 @@ using UpsertSampleWebApplication.Queries;
 
 namespace UpsertSampleWebApplication.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class TodoController : ControllerBase
+    public class TodoController : ApiControllerBase
     {
-        private readonly IMediator _mediator;
-
-        public TodoController(IMediator mediator)
-        {
-            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-        }
+        public TodoController(IMediator mediator) 
+            : base(mediator)
+        { }
         
         [HttpGet]
         public async Task<IActionResult> Get() => 
-            Ok(await _mediator.Send(new GetAllTodosQuery()));
+            Ok(await Mediator.Send(new GetAllTodosQuery()));
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] InsertTodoCommand command) => 
-            Ok(await _mediator.Send(command));
+            Ok(await Mediator.Send(command));
 
         [HttpPut("{id:guid}")]
-        public async Task<IActionResult> UpdateOwner(Guid id, [FromBody] UpdateTodoCommand command)
-        {
-            if (id != command.Id)
-            {
-                return BadRequest();
-            }
-
-            return Ok(await _mediator.Send(command));
-        }
+        public async Task<IActionResult> Put(Guid id, [FromBody] UpdateTodoCommand command) =>
+            id == command.Id
+                ? Ok(await Mediator.Send(command))
+                : BadRequest();
     }
 }
